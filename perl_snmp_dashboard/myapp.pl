64f1d63ec6264a54bf5d2bbf2fbc96c44e9e0fb5
Daniel Hansen <danielhansen1203@msn.com>
2025-04-26 12:31:46 +0200

    Update myapp.pl

--- perl_snmp_dashboard/myapp.pl
+++ perl_snmp_dashboard/myapp.pl
@@ -1,50 +1,11 @@
 #!/usr/bin/env perl
+use strict;
+use warnings;
 use Mojolicious::Lite;
-use lib 'lib';
-use MyApp::Device::Switch;
 
-# Basic route
-get '/' => {text => 'Perl SNMP Dashboard is running 🐪'};
-
-# REST endpoint for switch ports
-get '/api/switch' => sub {
-    my $c = shift;
-    my $switch = MyApp::Device::Switch->new(
-        host      => '192.168.1.1',      # <- Skift til din switches IP
-        community => 'public'           # <- Eller dit community string
-    );
-
-    my $ports = eval { $switch->get_port_names };
-    if ($@) {
-        $c->render(status => 500, json => {error => "SNMP error: $@"});
-    } else {
-        $c->render(json => $ports);
-    }
+get '/' => sub {
+  my $c = shift;
+  $c->render(text => 'Hello from Perl & Mojolicious!');
 };
 
-# GraphQL plugin
-plugin GraphQL => {
-  schema => {
-    query => {
-      switchPorts => {
-        type => ['Port'],
-        resolve => sub {
-          my $switch = MyApp::Device::Switch->new(
-              host => '192.168.1.1',
-              community => 'public'
-          );
-          my $ports = $switch->get_port_names;
-          return [ map { { oid => $_, name => $ports->{$_} } } keys %$ports ];
-        }
-      }
-    }
-  },
-  types => {
-    Port => {
-      oid  => 'String',
-      name => 'String'
-    }
-  }
-};
-
-app->start;
+app->start('daemon', '-l', 'http://*:3000');
