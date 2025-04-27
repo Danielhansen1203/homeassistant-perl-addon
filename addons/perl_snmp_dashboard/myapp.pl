ca67a23a4f84acc8100d435312e83e78df7e573a
Daniel Hansen <danielhansen1203@msn.com>
2025-04-27 02:33:07 +0200

    GIT -> HA!

--- addons/perl_snmp_dashboard/myapp.pl
+++ addons/perl_snmp_dashboard/myapp.pl
@@ -5,7 +5,7 @@ use Mojolicious::Lite;
 
 get '/' => sub {
   my $c = shift;
-  $c->render(text => 'Hello from GIT!');
+  $c->render(text => 'Det ser ud til at det bare spiller!!');
 };
 
 app->start('daemon', '-l', 'http://*:3000');
